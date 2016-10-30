c3a389fd8f0b11f41f356b213f18155a278f9337
Ethan Gould <ethan.g.907@gmail.com>
2016-10-30 18:33:44 -0400

    the game can detect a win vertically

--- scripts/app.js
+++ scripts/app.js
@@ -2,11 +2,19 @@
 
 $(document).ready(function() {
 	var module = {
-		moveCount: 0
+		moveCount: 0,
+		rows: $('.board__row'),
+		cells: $('.board__cell'),
+		board: { rows: [] }
 	};
 
+	// Add cells to board rows obj.
+	module.board.rows[0] = { row: module.rows[0], cells: $(module.rows[0]).children() };
+	module.board.rows[1] = { row: module.rows[1], cells: $(module.rows[1]).children() };
+	module.board.rows[2] = { row: module.rows[2], cells: $(module.rows[2]).children() };
+
 	module.eventHandlers = function() {
-		$('.board__cell').on('click', this.makeMove);
+		module.cells.on('click', this.makeMove);
 	};
 
 	module.makeMove = function(event) {
@@ -16,30 +24,51 @@ $(document).ready(function() {
 
 		// After the 5th move, start watching for a winner.
 		if (module.moveCount >= 4) {
-			module.checkForWinner();
+			module.checkForWinner(move);
 		}
-		// Increment moveCount
+		// Increment moveCount.
 		module.moveCount++;
 	}
 
-	module.checkForWinner = function() {
-		console.log('chcking for winner!', module.moveCount);
+	module.checkForWinner = function(move) {
+		// Check each row for a hirizontal win.
+		module.checkHorizontal(move);
 
-		$.each($('.board__row'), function(index, row) {
+		// Check each column for vertical win.
+		$.each([0,1,2], function(index, column) {
+			module.checkVertical(move, column);
+		});
+	};
 
-			// Does 'x' have 3 in a row, horizontally?
-			if ( $(row).children('.x').length === 3 ) {
-				$(row).addClass('winner');
-				console.log('x has won horitzontally', row);
-				return false;
-
-			// Does 'o' have 3 in a row, horizontally?
-			} else if ( $(row).children('.o').length === 3 ) {
-				$(row).addClass('winner');
-				console.log('o has won horitzontally', row);
+	module.checkHorizontal = function(move) {
+		// Does x/o have 3 in a row, horizontally?
+		$.each(module.rows, function(index, row) {
+			if ( $(row).children('.' + move).length === 3 ) {
+				$(row).children().addClass('winner');
 				return false;
 			}
 		});
+	};
+
+	module.checkVertical = function(move, column) {
+		var verticalCount = 0;
+		// Check each row of the provided column.
+		$.each(module.board.rows, function(index, row) {
+			// If the cell of the column currently being checked is
+			// already marked by the current users move (x/o) increment count.
+			if ( $( $(row.cells[column])[0]).hasClass(move) ) {
+				verticalCount++;
+			}
+		});
+
+		// If we find 3 in a column, we have a winner vertically.
+		if (3 === verticalCount) {
+			$.each(module.board.rows, function(index, row) {
+				// Marked the winning column's cells as winning cell.
+				$(row.cells[column]).addClass('winner');
+				module.cells.addClass('disabled');
+			});
+		}
 	}
 
 	module.eventHandlers();
