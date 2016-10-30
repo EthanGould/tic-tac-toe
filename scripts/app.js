efa88ddb87c505b051e47635137aaf0937c2d343
Ethan Gould <ethan.g.907@gmail.com>
2016-10-30 19:30:46 -0400

    reset functionality

--- scripts/app.js
+++ scripts/app.js
@@ -5,6 +5,7 @@ $(document).ready(function() {
 		moveCount: 0,
 		rows: $('.board__row'),
 		cells: $('.board__cell'),
+		reset: $('.board__reset'),
 		board: { rows: [] }
 	};
 
@@ -15,25 +16,26 @@ $(document).ready(function() {
 
 	module.eventHandlers = function() {
 		module.cells.on('click', this.makeMove);
+		module.reset.on('click', this.resetBoard);
 	};
 
 	module.makeMove = function(event) {
 		var $cell = $(event.target);
 		var move = module.moveCount % 2 === 0 ? 'x' : 'o';
-		
+
 		if ( !$cell.is('.disabled') ) {
 			$cell.addClass(move);
 			// Increment move count to keep track of whos turn it is.
 			module.moveCount++;
 		};
 
+		// Disabled clicks on cells that have been played.
 		$cell.addClass('disabled');
 
 		// After the 5th move, start watching for a winner.
-		if (module.moveCount >= 4) {
+		if (module.moveCount > 4) {
 			module.checkForWinner(move);
 		}
-		
 	}
 
 	module.checkForWinner = function(move) {
@@ -41,11 +43,12 @@ $(document).ready(function() {
 		$.each([0,1,2], function(index, column) {
 			module.checkVertical(move, column);
 		});
-
 		// Check each row for a hirizontal win.
 		module.checkHorizontal(move);
-		// Check each column for diagonal win.
-		module.checkDiagonal(move);
+		// Check each column from top left for diagonal win.
+		module.checkDiagonalLeft(move);
+		// Check each column from top right for diagonal win.
+		module.checkDiagonalRight(move);
 	};
 
 	module.checkHorizontal = function(move) {
@@ -72,29 +75,56 @@ $(document).ready(function() {
 		// If we find 3 in a column, we have a winner vertically.
 		if (3 === verticalCount) {
 			$.each(module.board.rows, function(index, row) {
-				// Marked the winning column's cells as winning cell.
-				$(row.cells[column]).addClass('winner');
-				module.cells.addClass('disabled');
+				module.showWinner(column, row);
 			});
 		}
 	}
 
-	module.checkDiagonal = function(move) {
+	module.checkDiagonalLeft = function(move) {
 		var diagonalCount = 0
-		// Does x/o have 3 in a row, horizontally?
+		// Does x/o have 3 in a row, diagonally?
 		$.each(module.rows, function(index, row) {
 			if ( $(row.cells[index]).hasClass(move) ) {
 				diagonalCount++;
 			}
 		});
 
+		// Highlight winning cells.
 		if (3 === diagonalCount) {
 			$.each(module.rows, function(index, row) {
-				// Marked the winning column's cells as winning cell.
-				$(row.cells[index]).addClass('winner');
-				module.cells.addClass('disabled');
+				module.showWinner(index, row);
 			});
 		}
+	};
+
+	module.checkDiagonalRight = function(move) {
+		var diagonalCount = 0
+		// Does x/o have 3 in a row, diagonally?
+		$.each(module.rows.toArray().reverse(), function(index, row) {
+			if ( $(row.cells[index]).hasClass(move) ) {
+				diagonalCount++;
+			}
+		});
+
+		// Highlight winning cells.
+		if (3 === diagonalCount) {
+			$.each(module.rows.toArray().reverse(), function(index, row) {
+				module.showWinner(index, row);
+			});
+		}
+	};
+
+
+	module.showWinner = function(index, row) {
+		// Marked the winning column's cells as winning cell.
+		// Disabled all other cells.
+		$(row.cells[index]).addClass('winner');
+		module.cells.addClass('disabled');
+		console.log('cells disabled');
+	};
+
+	module.resetBoard = function() {
+		module.cells.removeClass('x o disabled winner');
 	};
 
 	module.eventHandlers();
